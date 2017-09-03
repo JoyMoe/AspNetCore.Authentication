528d5ccfd734403b3b356afa3a6aff4fbad55368
7IN0SAN9 <me@7in0.me>
2017-09-03 18:14:41 +0800

    cleanup

--- src/JoyMoe.AspNetCore.Authentication.OpenId.Steam/SteamHandler.cs
+++ src/JoyMoe.AspNetCore.Authentication.OpenId.Steam/SteamHandler.cs
@@ -24,95 +24,95 @@ using JoyMoe.AspNetCore.Authentication.OpenID;
 
 namespace JoyMoe.AspNetCore.Authentication.OpenID.Steam
 {
-    public class SteamHandler : OpenIdHandler<SteamOptions>
-    {
+	public class SteamHandler : OpenIdHandler<SteamOptions>
+	{
 		public SteamHandler(IOptionsMonitor<SteamOptions> options, ILoggerFactory logger, UrlEncoder encoder, ISystemClock clock)
-            : base(options, logger, encoder, clock)
-        { }
+			: base(options, logger, encoder, clock)
+		{ }
 
 		protected override async Task<AuthenticationTicket> CreateTicketAsync(
 			ClaimsIdentity identity, AuthenticationProperties properties,
 			string identifier, IDictionary<string, string> attributes)
 		{
-            var principal = new ClaimsPrincipal(identity);
-            var ticket = new AuthenticationTicket(principal, properties, Options.AuthenticationScheme);
+			var principal = new ClaimsPrincipal(identity);
+			var ticket = new AuthenticationTicket(principal, properties, Options.AuthenticationScheme);
 
-            // Return the authentication ticket as-is if the
-            // user information endpoint has not been set.
-            if (string.IsNullOrEmpty(Options.UserInformationEndpoint))
-            {
-                Logger.LogInformation("The userinfo request was skipped because no userinfo endpoint was configured.");
+			// Return the authentication ticket as-is if the
+			// user information endpoint has not been set.
+			if (string.IsNullOrEmpty(Options.UserInformationEndpoint))
+			{
+				Logger.LogInformation("The userinfo request was skipped because no userinfo endpoint was configured.");
 
-                return ticket;
-            }
+				return ticket;
+			}
 
-            // Return the authentication ticket as-is
-            // if the application key has not been set.
-            if (string.IsNullOrEmpty(Options.ApplicationKey))
-            {
-                Logger.LogInformation("The userinfo request was skipped because no application key was configured.");
+			// Return the authentication ticket as-is
+			// if the application key has not been set.
+			if (string.IsNullOrEmpty(Options.ApplicationKey))
+			{
+				Logger.LogInformation("The userinfo request was skipped because no application key was configured.");
 
-                return ticket;
-            }
+				return ticket;
+			}
 
-            // Return the authentication ticket as-is if the claimed identifier is malformed.
-            if (!identifier.StartsWith(SteamConstants.Namespaces.Identifier, StringComparison.Ordinal))
-            {
-                Logger.LogWarning("The userinfo request was skipped because an invalid identifier was received: {Identifier}.", identifier);
+			// Return the authentication ticket as-is if the claimed identifier is malformed.
+			if (!identifier.StartsWith(SteamConstants.Namespaces.Identifier, StringComparison.Ordinal))
+			{
+				Logger.LogWarning("The userinfo request was skipped because an invalid identifier was received: {Identifier}.", identifier);
 
-                return ticket;
-            }
+				return ticket;
+			}
 
-            var address = QueryHelpers.AddQueryString(Options.UserInformationEndpoint, new Dictionary<string, string>
-            {
-                [SteamConstants.Parameters.Key] = Options.ApplicationKey,
-                [SteamConstants.Parameters.SteamId] = identifier.Substring(SteamConstants.Namespaces.Identifier.Length)
-            });
+			var address = QueryHelpers.AddQueryString(Options.UserInformationEndpoint, new Dictionary<string, string>
+			{
+				[SteamConstants.Parameters.Key] = Options.ApplicationKey,
+				[SteamConstants.Parameters.SteamId] = identifier.Substring(SteamConstants.Namespaces.Identifier.Length)
+			});
 
-            var request = new HttpRequestMessage(HttpMethod.Get, address);
-            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(OpenIdConstants.Media.Json));
+			var request = new HttpRequestMessage(HttpMethod.Get, address);
+			request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue(OpenIdConstants.Media.Json));
 
-            // Return the authentication ticket as-is if the userinfo request failed.
-            var response = await Options.HttpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
-            if (!response.IsSuccessStatusCode)
-            {
-                Logger.LogWarning("The userinfo request failed because an invalid response was received: the identity provider " +
-                                  "returned returned a {Status} response with the following payload: {Headers} {Body}.",
-                                  /* Status: */ response.StatusCode,
-                                  /* Headers: */ response.Headers.ToString(),
-                                  /* Body: */ await response.Content.ReadAsStringAsync());
+			// Return the authentication ticket as-is if the userinfo request failed.
+			var response = await Options.HttpClient.SendAsync(request, HttpCompletionOption.ResponseHeadersRead, Context.RequestAborted);
+			if (!response.IsSuccessStatusCode)
+			{
+				Logger.LogWarning("The userinfo request failed because an invalid response was received: the identity provider " +
+								  "returned returned a {Status} response with the following payload: {Headers} {Body}.",
+								  /* Status: */ response.StatusCode,
+								  /* Headers: */ response.Headers.ToString(),
+								  /* Body: */ await response.Content.ReadAsStringAsync());
 
-                return ticket;
-            }
+				return ticket;
+			}
 
-            var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
+			var payload = JObject.Parse(await response.Content.ReadAsStringAsync());
 
-            // Try to extract the profile name of the authenticated user.
-            var profile = payload.Value<JObject>(SteamConstants.Parameters.Response)
-                                ?.Value<JArray>(SteamConstants.Parameters.Players)
-                            ?[0]?.Value<string>(SteamConstants.Parameters.Name);
+			// Try to extract the profile name of the authenticated user.
+			var profile = payload.Value<JObject>(SteamConstants.Parameters.Response)
+								?.Value<JArray>(SteamConstants.Parameters.Players)
+							?[0]?.Value<string>(SteamConstants.Parameters.Name);
 
-            if (!string.IsNullOrEmpty(profile))
-            {
-                identity.AddClaim(new Claim(ClaimTypes.Name, profile, ClaimValueTypes.String, Options.ClaimsIssuer));
-            }
+			if (!string.IsNullOrEmpty(profile))
+			{
+				identity.AddClaim(new Claim(ClaimTypes.Name, profile, ClaimValueTypes.String, Options.ClaimsIssuer));
+			}
 
-            var context = new OpenIdContext(Context, Scheme, Options, ticket)
-            {
-                User = payload
-            };
+			var context = new OpenIdContext(Context, Scheme, Options, ticket)
+			{
+				User = payload
+			};
 
-            // Copy the attributes to the context object.
-            foreach (var attribute in attributes)
-            {
-                context.Attributes.Add(attribute);
-            }
+			// Copy the attributes to the context object.
+			foreach (var attribute in attributes)
+			{
+				context.Attributes.Add(attribute);
+			}
 
-            await Options.Events.Authenticated(context);
+			await Options.Events.Authenticated(context);
 
-            // Note: return the authentication ticket associated
-            // with the notification to allow replacing the ticket.
-            return context.Ticket;
-        }
-    }
+			// Note: return the authentication ticket associated
+			// with the notification to allow replacing the ticket.
+			return context.Ticket;
+		}
+	}
 }
